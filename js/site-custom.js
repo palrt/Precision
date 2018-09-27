3269ac9b66ec87e92cb268254ed9a6f36d7c6c60
Ankit Raturi <keshavpal9@gmail.comgit config --global user.email keshavpal9@gmail.com>
2018-09-27 17:46:37 +0530

    first commit

---
+++ js/site-custom.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function(){
+	jQuery('.menubtn').on('click', function(){
+	jQuery('.header-nav-main  .main-nav').slideToggle();
+	if(jQuery('.header-nav-main .main-nav').is(':visible')){
+		jQuery('body').toggleClass('responsive-menu-open');
+	}
+	});
+	
+	jQuery(".lazy").slick({
+        infinite: true,
+		dots: true,
+		fade: true,
+		autoplay: true,
+		lazyLoad: 'ondemand',
+		adaptiveHeight: true
+      });
+	  
+	  jQuery(".smile").slick({
+        infinite: true,
+		dots: true,
+		autoplay: true,
+		fade: true,
+		lazyLoad: 'ondemand',
+		adaptiveHeight: true
+      });
+	  
+	  
+	  jQuery('.bla-1').YouTubePopUp();
+	  
+	  jQuery('.header-nav-main .main-nav li a[href^="#"]').click(function(e){
+		  jQuery('html,body').animate({
+			  scrollTop:jQuery(this.hash).offset().top
+			},1000);
+		jQuery('.header-nav-main .main-nav li a').removeClass('active');
+		jQuery(this).addClass('active');
+		return false;e.preventDefault();
+		});
+});
